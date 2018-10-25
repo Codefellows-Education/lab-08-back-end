1e85ce60c31c370fc1efd90fc00d94241fd48c40
Charles Clemens <cclemensjr@gmail.com>
2018-10-25 11:28:56 -0700

    Added server logic to location data

--- server.js
+++ server.js
@@ -17,44 +17,60 @@ const app = express();
 
 app.use(cors());
 
+app.get('/location', getLocation);
+app.get('/weather', getWeather);
+app.get('/yelp', getYelp);
+app.get('/movies', getMovies);
 
-
-//This will take the location name and run the searchtolatlong() which will store the location information as an object that contains latitude,longitude and location name.
-app.get('/location', (request, response) => {
-  // console.log('GET /location', request.query);
-  
-  //runs the searchtolatlong() which takes in the query data from the URL.
-  searchToLatLong(request.query.data)
-    .then( locationData => {//location data is the superagent return
-      response.send(locationData);
-    })
-    //This will handle our errors
-    .catch ( error => handleError(error,response));
-});
-
-app.listen(PORT, () => console.log(`App is up on http://localhost:${PORT}`));
-
-//This function takes in the query and makes the request to the API,then format the data that it gets into the object that we need.
-function searchToLatLong(query) {
-  const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
-  // console.log('getting',URL);
-
-  return superagent.get(URL)
-    .then( data => {
-      let location = new Location(data.body.results[0]);
-
-      //This line fills in the Actual search query to the object.
-      location.search_query = query;
-      return location;
-    })
-    .catch(error => handleError(error));
-}
-
-function Location(data) {
+function Location(query, data) {
+  this.search_query = query;
   this.formatted_query = data.formatted_address;
   this.latitude = data.geometry.location.lat;
   this.longitude = data.geometry.location.lng;
 }
+
+Location.prototype.save = function () {
+  let SQL = `INSERT INTO locations(search_query, formatted_query, latitude, longitude) VALUES($1, $2, $3, $4)`;
+  let values = Object.values(this);
+
+  client.query(SQL, values);
+}
+
+Location.fetchLocations = (query) => {
+  const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+  console.log('Got the url: ', URL);
+}
+
+//This will take the location name and run the searchtolatlong() which will store the location information as an object that contains latitude,longitude and location name.
+// app.get('/location', (request, response) => {
+//   // console.log('GET /location', request.query);
+  
+//   //runs the searchtolatlong() which takes in the query data from the URL.
+//   searchToLatLong(request.query.data)
+//     .then( locationData => {//location data is the superagent return
+//       response.send(locationData);
+//     })
+//     //This will handle our errors
+//     .catch ( error => handleError(error,response));
+// });
+
+app.listen(PORT, () => console.log(`App is up on http://localhost:${PORT}`));
+
+//This function takes in the query and makes the request to the API,then format the data that it gets into the object that we need.
+// function searchToLatLong(query) {
+//   // console.log('getting',URL);
+
+//   return superagent.get(URL)
+//     .then( data => {
+//       let location = new Location(data.body.results[0]);
+
+//       //This line fills in the Actual search query to the object.
+//       location.search_query = query;
+//       return location;
+//     })
+//     .catch(error => handleError(error));
+// }
+
 
 
 /////weather
